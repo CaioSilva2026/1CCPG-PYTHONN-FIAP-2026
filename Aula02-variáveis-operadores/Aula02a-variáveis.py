5c5c6a3d2ad5770aa7848b9215ef9ed0346ed9a3
CaioSilva2026 <chferrazsilva2@gmail.com>
2026-03-19 11:42:28 -0300

    aula 02 variáveis

---
+++ Aula02-variáveis-operadores/Aula02a-variáveis.py
@@ -0,0 +1,42 @@
+print("ola mundo")
+
+print("7 + 4")
+print("7" + "4")#CONCATENANDO STRINGS
+
+# Comentário de 1 linha
+'''
+    Comentários de 
+    multiplas
+    linhas
+'''
+
+
+#váriaves
+nome ="Caio" # str
+idade = 26 # int
+peso = 70.2 # float
+
+print(nome, idade, peso)
+print("oiii \n", nome, idade, peso)
+print(f"Oii, {nome}!!!")
+
+#INPUT -- SIMULAÇÃO DE UM FORMS NO CMD
+nome = input("Digite o seu nome")
+print(nome)
+idade = int(input("Digite sua idade"))
+peso = input("Digite seu peso")
+
+print(nome, idade, peso)
+print(idade + 1)
+
+ano_nascimento = 2006
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print(f"Sua idade é: {idade}")
+
+
+
+
+
+
+
